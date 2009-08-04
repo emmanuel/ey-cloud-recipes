7d2c4bfdfcf2ca783b01e2f259b41de2b091cd3b
Ezra Zygmuntowicz <ez@engineyard.com>
2009-08-04 12:20:44 -0700

    only run on proper roles

--- cookbooks/sphinx/recipes/default.rb
+++ cookbooks/sphinx/recipes/default.rb
@@ -5,57 +5,62 @@ require 'pp'
 #
 #if_app_needs_recipe("thinking_sphinx") do |app,data,index|
 
-run_for_app("ThinkingAboutSphinx") do |app_name, data|
 
-  directory "/var/run/sphinx" do
-    owner node[:owner_name]
-    group node[:owner_name]
-    mode 0755
-  end
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
 
-  directory "/var/log/engineyard/sphinx/#{app_name}" do
-    recursive true
-    owner node[:owner_name]
-    group node[:owner_name]
-    mode 0755
-  end
+  run_for_app("ThinkingAboutSphinx") do |app_name, data|
   
-  remote_file "/etc/logrotate.d/sphinx" do
-    owner "root"
-    group "root"
-    mode 0755
-    source "sphinx.logrotate"
-    action :create
-  end
-
-  template "/etc/monit.d/sphinx.#{app_name}.monitrc" do
-      source "sphinx.monitrc.erb"
+    directory "/var/run/sphinx" do
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0755
+    end
+  
+    directory "/var/log/engineyard/sphinx/#{app_name}" do
+      recursive true
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0755
+    end
+    
+    remote_file "/etc/logrotate.d/sphinx" do
+      owner "root"
+      group "root"
+      mode 0755
+      source "sphinx.logrotate"
+      action :create
+    end
+  
+    template "/etc/monit.d/sphinx.#{app_name}.monitrc" do
+        source "sphinx.monitrc.erb"
+        owner node[:owner_name]
+        group node[:owner_name]
+        mode 0644
+        variables({
+          :app => app_name,
+          :user => node[:owner_name]
+        })
+    end
+  
+    template "/data/#{app_name}/shared/config/sphinx.yml" do
       owner node[:owner_name]
       group node[:owner_name]
       mode 0644
+      source "sphinx.yml.erb"
       variables({
         :app => app_name,
         :user => node[:owner_name]
       })
-  end
-
-  template "/data/#{app_name}/shared/config/sphinx.yml" do
-    owner node[:owner_name]
-    group node[:owner_name]
-    mode 0644
-    source "sphinx.yml.erb"
-    variables({
-      :app => app_name,
-      :user => node[:owner_name]
-    })
+    end
+    
+    link "/data/#{app_name}/current/config/sphinx.yml" do
+      to "/data/#{app_name}/shared/config/sphinx.yml"
+    end
+  
+    link "/data/#{app_name}/current/config/thinkingsphinx" do
+      to "/data/#{app_name}/shared/config/thinkingsphinx"
+    end
+  
   end
   
-  link "/data/#{app_name}/current/config/sphinx.yml" do
-    to "/data/#{app_name}/shared/config/sphinx.yml"
-  end
-
-  link "/data/#{app_name}/current/config/thinkingsphinx" do
-    to "/data/#{app_name}/shared/config/thinkingsphinx"
-  end
-
 end
