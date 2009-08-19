812c91e7294cae6009dbf0b1d2c065f9b5e2c4e0
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2009-08-18 17:37:38 -0700

    Added ey-cloud recipe for delayed_job.

---
+++ cookbooks/delayed_job/recipes/default.rb
@@ -0,0 +1,52 @@
+#
+# Cookbook Name:: delayed_job
+# Recipe:: default
+#
+
+# run DelayedJob worker on app instances
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+
+  directory "/var/run/delayed_job" do
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0755
+  end
+
+  directory "/var/log/engineyard/delayed_job/#{app_name}" do
+    recursive true
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0755
+  end
+
+  remote_file "/etc/logrotate.d/delayed_job" do
+    owner "root"
+    group "root"
+    mode 0755
+    source "delayed_job.logrotate"
+    action :create
+  end
+
+  template "/data/#{app_name}/current/config/delayed_job.yml" do
+    source "delayed_job.yml.erb"
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0644
+    variables({
+      :app_name => app_name,
+      :user => node[:owner_name]
+    })
+  end
+
+  template "/etc/monit.d/delayed_job.#{app_name}.monitrc" do
+    source "delayed_job.monitrc.erb"
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0644
+    variables({
+      :app_name => app_name,
+      :user => node[:owner_name]
+    })
+  end
+
+end
