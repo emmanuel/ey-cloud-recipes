5ada63411cc207d3ff57682b8b0480276def484b
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2009-08-20 16:26:57 -0700

    Updated recipes to use new #app_name shortcut (will prevent breakage when moving from "NavigatingCancer" (staging/testing) to beta).

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -12,7 +12,7 @@ if ['solo', 'app', 'app_master'].include?(node[:instance_role])
     mode 0755
   end
 
-  directory "/var/log/engineyard/delayed_job/#{node[:environment][:name]}" do
+  directory "/var/log/engineyard/delayed_job/#{app_name}" do
     recursive true
     owner node[:owner_name]
     group node[:owner_name]
@@ -27,24 +27,24 @@ if ['solo', 'app', 'app_master'].include?(node[:instance_role])
     action :create
   end
 
-  template "/data/#{node[:environment][:name]}/shared/config/delayed_job.yml" do
+  template "/data/#{app_name}/shared/config/delayed_job.yml" do
     source "delayed_job.yml.erb"
     owner node[:owner_name]
     group node[:owner_name]
     mode 0644
     variables({
-      :app_name => node[:environment][:name],
+      :app_name => app_name,
       :user => node[:owner_name]
     })
   end
 
-  template "/etc/monit.d/delayed_job.#{node[:environment][:name]}.monitrc" do
+  template "/etc/monit.d/delayed_job.#{app_name}.monitrc" do
     source "delayed_job.monitrc.erb"
     owner node[:owner_name]
     group node[:owner_name]
     mode 0644
     variables({
-      :app_name => node[:environment][:name],
+      :app_name => app_name,
       :user => node[:owner_name]
     })
   end
