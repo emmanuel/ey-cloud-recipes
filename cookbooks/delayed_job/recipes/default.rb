f9bc61a7116d0e8c5257a8b98ea8452e0e829872
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2009-08-20 19:01:10 -0700

    Made delayed_job worker process name a little more informative.

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -6,6 +6,8 @@
 # run DelayedJob worker on app instances
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
   app_name = node[:applications].keys.first
+  rails_env = node[:environment][:framework_env]
+  worker_name = "delayed_job_worker_#{app_name}_#{rails_env}"
 
   directory "/var/run/delayed_job" do
     owner node[:owner_name]
@@ -35,17 +37,21 @@ if ['solo', 'app', 'app_master'].include?(node[:instance_role])
     mode 0644
     variables({
       :app_name => app_name,
+      :rails_env => rails_env,
+      :worker_name => worker_name,
       :user => node[:owner_name]
     })
   end
 
-  template "/etc/monit.d/delayed_job.#{app_name}.monitrc" do
-    source "delayed_job.monitrc.erb"
+  template "/etc/monit.d/#{worker_name}.monitrc" do
+    source "delayed_job_worker.monitrc.erb"
     owner "root"
     group "root"
     mode 0644
     variables({
       :app_name => app_name,
+      :rails_env => rails_env,
+      :worker_name => worker_name,
       :user => node[:owner_name]
     })
   end
