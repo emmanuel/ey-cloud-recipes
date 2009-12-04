a24bbb9881073f3d114573aec678903f6d683555
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2009-12-03 20:33:44 -0800

    Minimal recipe to generate a Whenever config with the current environment.

---
+++ cookbooks/whenever/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: whenever
+# Recipe:: default
+#
+# Whenever defaults to production mode, so we generate a config file
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+  app_name = "NavigatingCancer"
+
+  template "/data/#{app_name}/current/config/schedule.rb" do
+    source "schedule.rb.erb"
+    owner "navi"
+    group "navi"
+    mode 0644
+    variables({
+      :app_name => app_name,
+      :rails_env => @node[:environment][:framework_env],
+    })
+  end
+
+end
