67f81c5f8c5773923dde29aa183dd74ccaf01f9e
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2009-12-03 21:27:50 -0800

    Try to template schedule.rb into /data/#{app_name}/shared/config.

--- cookbooks/whenever/recipes/default.rb
+++ cookbooks/whenever/recipes/default.rb
@@ -6,13 +6,20 @@
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
   app_name = "NavigatingCancer"
 
-  template "/data/#{app_name}/shared/schedule.rb" do
+  directory "/data/#{app_name}/shared/config" do
+    recursive true
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0755
+  end
+
+  template "/data/#{app_name}/shared/config/schedule.rb" do
     source "schedule.rb.erb"
     owner "navi"
     group "navi"
     mode 0644
     variables({
-      :app_name => app_name,
+      :app_name  => app_name,
       :rails_env => @node[:environment][:framework_env],
     })
   end
